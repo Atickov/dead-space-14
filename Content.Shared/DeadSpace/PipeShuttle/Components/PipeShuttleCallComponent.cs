e1803f6a8d95ac4a4e0566db1ec0302e78ba1a2a
Genes1us <168219101+Genes1us@users.noreply.github.com>
2026-08-31 21:13:37 +0500

    МЕРДЖ КОНФЛИКТ УДАЛИЛ АО

--- Content.Shared/DeadSpace/PipeShuttle/Components/PipeShuttleCallComponent.cs
+++ Content.Shared/DeadSpace/PipeShuttle/Components/PipeShuttleCallComponent.cs
@@ -1,17 +1,7 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
-using Content.Shared.Roles.Components;
 using Robust.Shared.GameStates;
 
-<<<<<<<< HEAD:Content.Shared/DeadSpace/Hooligan/Roles/HooliganRoleComponent.cs
-namespace Content.Shared.DeadSpace.Hooligan.Roles;
-
-/// <summary>
-/// Вешается на сущность разума игрока.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class HooliganRoleComponent : BaseMindRoleComponent;
-========
 namespace Content.Shared.DeadSpace.PipeShuttle.Components;
 
 [RegisterComponent, NetworkedComponent]
@@ -20,4 +10,3 @@ public sealed partial class PipeShuttleCallComponent : Component
     [DataField]
     public EntityUid? Shuttle;
 }
->>>>>>>> master:Content.Shared/DeadSpace/PipeShuttle/Components/PipeShuttleCallComponent.cs
