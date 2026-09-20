fb4ea858310e9e83bf4aa0456702d687560484d1
Atickov <atickov222@gmail.com>
2026-09-20 13:25:49 +0300

    конфликты2

--- Content.Server/Medical/CrewMonitoring/CrewMonitoringConsoleComponent.cs
+++ Content.Server/Medical/CrewMonitoring/CrewMonitoringConsoleComponent.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Medical.SuitSensor;
+using Content.Shared.Medical.CrewMonitoring;
 
 namespace Content.Server.Medical.CrewMonitoring;
 
@@ -35,13 +36,3 @@ public sealed partial class CrewMonitoringConsoleComponent : Component
     public CrewMonitoringConsolePingMode CurrentPingMode = CrewMonitoringConsolePingMode.Severe;
     // DS14-end
 }
-
-// DS14-start
-public enum CrewMonitoringConsolePingMode
-{
-    Severe,
-    Critical,
-    Dead,
-    Disabled
-}
-// DS14-end
